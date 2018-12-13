0f53d9930be999922d4a3972f039f2e080c5f06d
Andrey Sokolow <quick.sokolow94@gmail.com>
2018-12-13 17:32:57 +0300

    modal window completed

--- app/js/myscripts.js
+++ app/js/myscripts.js
@@ -41,5 +41,42 @@ $(document).ready(function() {
 			$(this).closest('.language-name').find('span').html(a);
 			a = a.split('',2).join('');
 			$(this).closest('.language-name').siblings('.language').html(a);
-		});	
+		});
+		$(document).on('keyup', '.modal-block__input', function() {
+		  let a = $(this);
+		  if ($(this).val().length > 0) {
+		    $(this).siblings('.modal-block__placeholder').addClass('active');
+		  } else {
+		    $(this).siblings('.modal-block__placeholder').removeClass('active');
+		  };
+		});
+		$(document).on('click', '.modal-block_flexbox', function(event) {
+		  event.preventDefault();
+		  $(this).children('.modal-block__checkbox-avatar').toggleClass('active');
+		  if ($(this).children('.modal-block__checkbox').is(':checked')) {
+		    $(this).children('.modal-block__checkbox').prop('checked', false);
+		  } else {
+		    $(this).children('.modal-block__checkbox').prop('checked', true);
+		  }
+		});
+
+		$(document).on('keyup', '.modal-block__input', function() {
+			let counter = 0;
+
+			$('.modal-block__input').each(function(){
+				if (!($(this).val() == '')) {
+					counter++;
+				}
+			});
+
+			if ($(this).index('.modal-block__input') >= 2 && $(this).val().length >= 6) {
+				counter++;
+			}
+
+			if (counter == 5) {
+				$('.main-btn.main-btn_wide.disabled').removeClass('disabled').prop('disabled', false);
+			} else {
+				$('.main-btn.main-btn_wide').addClass('disabled').prop('disabled', true);
+			};
+		});
 });
