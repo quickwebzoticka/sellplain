994676af0c94cfb0eddbb9a0584ceb9240201f50
Andrey Sokolow <quick.sokolow94@gmail.com>
2018-12-14 17:00:01 +0300

    finished sellplain

--- app/js/myscripts.js
+++ app/js/myscripts.js
@@ -50,33 +50,54 @@ $(document).ready(function() {
 		    $(this).siblings('.modal-block__placeholder').removeClass('active');
 		  };
 		});
+		function button(counter, counterCheckbox) {
+			console.log(counter);
+			console.log(counterCheckbox);
+
+			if (parseInt(counter + counterCheckbox) == 8) {
+				$('.main-btn.main-btn_wide.disabled').removeClass('disabled').prop('disabled', false);
+			} else {
+				$('.main-btn.main-btn_wide').addClass('disabled').prop('disabled', true);
+			};
+		}
+		var counterCheckbox = 0;
+		var counter = 0;
+		$(document).on('keyup', '.modal-block__input', function() {
+			counter = 0;
+			$('.modal-block__input').each(function(){
+				if (!($(this).val() == '')) {
+					counter++;
+				}
+				if ($(this).index('.modal-block__input') >= 2 && $(this).val().length >= 6) {
+					counter++;
+				}
+			});
+			if ($(this).closest('form').find('.modal-block__input').eq(2).val() == $(this).closest('form').find('.modal-block__input').eq(3).val() && $(this).val() !== undefined) {
+					counter++;
+				}
+			if ($('.modal-block__checkbox').prop('checked')) {
+		    	counter++;
+		    }
+		    counter + counterCheckbox;
+			button(counter, counterCheckbox);
+		});
+
 		$(document).on('click', '.modal-block_flexbox', function(event) {
 		  event.preventDefault();
 		  $(this).children('.modal-block__checkbox-avatar').toggleClass('active');
 		  if ($(this).children('.modal-block__checkbox').is(':checked')) {
 		    $(this).children('.modal-block__checkbox').prop('checked', false);
+		    counterCheckbox = 0;
+		    counter + counterCheckbox;
+		    button(counter, counterCheckbox);
 		  } else {
 		    $(this).children('.modal-block__checkbox').prop('checked', true);
+		    counterCheckbox = 1;
+		    counter + counterCheckbox;
+		    button(counter, counterCheckbox);
 		  }
 		});
+		
 
-		$(document).on('keyup', '.modal-block__input', function() {
-			let counter = 0;
 
-			$('.modal-block__input').each(function(){
-				if (!($(this).val() == '')) {
-					counter++;
-				}
-			});
-
-			if ($(this).index('.modal-block__input') >= 2 && $(this).val().length >= 6) {
-				counter++;
-			}
-
-			if (counter == 5) {
-				$('.main-btn.main-btn_wide.disabled').removeClass('disabled').prop('disabled', false);
-			} else {
-				$('.main-btn.main-btn_wide').addClass('disabled').prop('disabled', true);
-			};
-		});
 });
